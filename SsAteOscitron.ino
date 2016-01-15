464df930c5da3eda2fb11c4869316afcbfeaae4d
soulsbysynths <info@soulsbysynths.com>
2016-01-15 02:21:33 +0000

    Oscitron first commit, pre NAMM

---
+++ SsAteOscitron.ino
@@ -0,0 +1,37 @@
+#include <AteOsc.h>
+#include <AteOscEngineBase.h>
+#include <AteOscHardwareBase.h>
+#include <AtmAudio/AtmAudio.h>
+#include <Wavetable/Wavetable.h>
+#include <AteOscOscillator.h>
+#include <AteOscPatch.h>
+#include <SsHelpers.h>
+#include <BiquadFilter.h>
+#include <Portamento.h>
+#include <QuantizePitch.h>
+#include <AtmPitch.h>
+#include <Wavecrusher.h>
+#include <Flanger.h>
+#include <Pwm.h>
+#include <LedRgb.h>
+#include <LedCircular.h>
+#include <CvInput.h>
+#include <Switch.h>
+#include <RotaryEncoder.h>
+
+AteOsc oscitron;
+
+void setup()
+{
+
+	oscitron.initialize();
+
+}
+
+void loop()
+{
+	static unsigned long last_millis;
+	unsigned char ticksPassed = millis() - last_millis;
+	last_millis += ticksPassed;
+	oscitron.poll(ticksPassed);
+}
